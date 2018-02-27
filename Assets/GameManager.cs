fafc4e22919492f6c821471e3778df57616180b0
jwardlaw <jwardlaw.n@gmail.com>
2018-02-27 07:53:20 -0800

    Initial Commit

---
+++ Assets/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+public class GameManager : MonoBehaviour {
+
+    public Player p1;
+    public Player p2;
+    public Player p3;
+    public Player p4;
+    
+
+	// Use this for initialization
+	void Start () {
+
+        p1 = ReInput.players.GetPlayer(0);
+
+    }
+
+    void swapControls()
+    {
+        foreach (Joystick joystick in p1.controllers.Joysticks)
+        {
+            p1.controllers.maps.LoadMap(ControllerType.Joystick, joystick.id, "Default", "Runner", true);
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            Debug.Log("Swap");
+            swapControls();
+        }
+		
+	}
+}
---
+++ Assets/RunnerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+public class RunnerMovement : MonoBehaviour {
+    private int playerId = 0;
+
+    private Player player;
+    private CharacterController cc;
+
+    private float moveSpeed = 3f;
+    private Vector3 moveDir;
+
+    void getInput()
+    {
+        moveDir.x = player.GetAxis("WalkHorizontal");
+    }
+
+    void processInput()
+    {
+        if (moveDir.x != 0.0f || moveDir.y != 0.0f)
+        {
+            Debug.Log("move");
+            cc.Move(moveDir * moveSpeed * Time.deltaTime);
+        }
+    }
+
+	// Use this for initialization
+	void Awake() {
+        player = ReInput.players.GetPlayer(playerId);
+
+        // Get the character controller
+        cc = GetComponent<CharacterController>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        getInput();
+        processInput();
+		
+	}
+}
